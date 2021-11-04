cd4eed2f2170fb881293874fcf58844b5df30cb1
Wen <wbai@deltacontrols.com>
2021-11-04 05:11:34 -0700

    cp4 continued

--- cp4_the_object_life_cycle/exp6_dynamic_storage_duration.cpp
+++ cp4_the_object_life_cycle/exp6_dynamic_storage_duration.cpp
@@ -7,4 +7,16 @@
  * You have manual control over when a dynamic object's life begins and when it ends.
  * Dynamic objects are also called allocated objects for this reason.
  *
+ * The primary way to allocate a dynamic object is with a new expression. A new expression begins
+ * with the new keyword followed by the desired type of the dynamic object. New expression create
+ * objects of a given type and then return a pointer to the newly minted object.
  */
+int main (){
+    int *my_int_ptr = new int;
+
+    int *my_other_int_ptr = new int{42};
+
+    delete my_int_ptr;
+
+    delete my_other_int_ptr;
+}
