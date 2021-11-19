ee8a68d1f9eca66c0db09f9c71de7ac5fb394c6f
wbai <wbai@deltacontrols.com>
2021-11-19 14:41:51 -0800

    cp7 cont...

--- cp7_expressions/exp16_cstyle_casts.cpp
+++ cp7_expressions/exp16_cstyle_casts.cpp
@@ -15,5 +15,24 @@
  *
  * The syntax of the C++ explicit casts is intentionally ugly and verbose. This calls attention to a point in the code
  * where the rigid rules of the type system are being bent or broken. The C-style cast doesn't do this. In addition,
- * it's not clear
+ * it's not clear from the cast what kind of conversion the programmer is intending. When you ue instruments like the
+ * named casts, the compiler can at least enforce some constraints. For example, it's all too easy to forget const
+ * correctness when using a C-style cast when you only intended a reinterpret_cast.
+ *
+ * Suppose you wanted to treat a const char* array as unsigned within the body of a function. It would be too easy to
+ * write following code.
  */
+
+#include <cstdio>
+
+void trainwreck(const char *read_only) {
+    auto as_unsigned = reinterpret_cast<unsigned char*> (read_only);
+    *as_unsigned = 'b'; // Crashes on Windows 10 x64
+}
+
+int main() {
+    auto ezra = "Ezra";
+    printf("Before trainwreck: %s\n", ezra);
+    trainwreck(ezra);
+    printf("After trainwreck: %s\n", ezra);
+}
