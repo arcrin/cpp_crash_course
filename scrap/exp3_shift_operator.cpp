259b8b7b96ec6c6fadbfc5d65c522fce652a9852
Wen <wbai@deltacontrols.com>
2021-12-20 20:24:11 -0800

    misc

--- scrap/exp3_shift_operator.cpp
+++ scrap/exp3_shift_operator.cpp
@@ -9,6 +9,8 @@ int main() {
     uint8_t temp{};
 //    temp = 1 << (2 * 1);
 //    temp = 2 * (1 << 1);
-    temp = 0x3 << (2 * 1);
-    printf("%d", temp);
+//    temp = 0x3 << (2 * 1); // shift left by 1, twice
+//    printf("%d", temp);
+    temp = 0x3;
+    printf("%d", (temp >> 2) & 0x00000001);
 }
