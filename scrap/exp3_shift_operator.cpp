943b132f2af2670a176cbe243e6a452b080cbd41
wbai <wbai@deltacontrols.com>
2021-12-21 17:38:54 -0800

    some exercises

--- scrap/exp3_shift_operator.cpp
+++ scrap/exp3_shift_operator.cpp
@@ -11,6 +11,14 @@ int main() {
 //    temp = 2 * (1 << 1);
 //    temp = 0x3 << (2 * 1); // shift left by 1, twice
 //    printf("%d", temp);
-    temp = 0x3;
-    printf("%d", (temp >> 2) & 0x00000001);
+//    temp = 0x3;
+//    printf("%d", (temp >> 2) & 0x00000001);
+    uint16_t temp16{0};
+    printf("%d\n", temp16);
+    temp16 |= (0x1 << 4);
+    printf("%d\n", temp16);
+    temp16 |= (0x1 << 5);
+    printf("%d\n", temp16);
+    temp16 &= ~(0x1 << 4);
+    printf("%d", temp16);
 }
---
+++ scrap/exp4_null_pointer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wbai on 12/21/2021.
+//
+
+int mani(){
+    *int p = null;
+    *p = 10; //this won't work as there is no memory backing up this pointer
+    p = new int;
+    *p = 10;
+}
