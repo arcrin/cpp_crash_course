96decc2fd656deeca89cfafbc2d5948883a625c1
wbai <wbai@deltacontrols.com>
2021-12-31 14:26:26 -0800

    misc.

--- scrap/exp3_shift_operator.cpp
+++ scrap/exp3_shift_operator.cpp
@@ -6,19 +6,22 @@
 #include <cstdio>
 
 int main() {
-    uint8_t temp{};
+//    uint8_t temp{};
 //    temp = 1 << (2 * 1);
 //    temp = 2 * (1 << 1);
 //    temp = 0x3 << (2 * 1); // shift left by 1, twice
 //    printf("%d", temp);
 //    temp = 0x3;
 //    printf("%d", (temp >> 2) & 0x00000001);
-    uint16_t temp16{0};
-    printf("%d\n", temp16);
+//    uint16_t temp16{0};
+//    printf("%d\n", temp16);
 //    temp16 |= (0x1 << 4);
 //    printf("%d\n", temp16);
 //    temp16 |= (0x1 << 5);
-    printf("%d\n", temp16);
-    temp16 &= ~(0x3 << 12 * 2);
-    printf("%d", (0x3 << 12 * 2));
+//    printf("%d\n", temp16);
+//    temp16 &= ~(0x3 << 12 * 2);
+//    printf("%d", (0x3 << 12 * 2));
+    uint32_t temp{0};
+    temp = (0x8 << 4) << (8 * 3);
+    printf("%u", temp);
 }
