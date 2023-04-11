53135f53c43a716a857fcf98acacf324605aadbc
Wen <wbai@deltacontrols.com>
2023-04-10 23:35:06 -0700

    cp6

--- object_natural/cp6/fig06_04.cpp
+++ object_natural/cp6/fig06_04.cpp
@@ -10,6 +10,15 @@ int main() {
     // constant can be used to specify array size
     constexpr size_t arraySize{5}; // must initialize in declaration
 
-    std::array<int, arraySize> values{}; // array values has 5 elements
+    std::array<size_t, arraySize> values{}; // array values has 5 elements
 
+    for (int i{0}; i < values.size(); ++i) {
+        values.at(i) = 2 + 2 * i;
+    }
+
+    // output contents of array values in tabular format
+    for (const size_t &value: values) {
+        std::cout << fmt::format("{} ", value);
+    }
+    std::cout << "\n";
 }
