9d75930e2047fc26cddcf0a096375454cd871b98
wbai <wbai@deltacontrols.com>
2023-09-21 16:24:04 -0700

    cont.

--- scrap/macro_advanced_test.cpp
+++ scrap/macro_advanced_test.cpp
@@ -2,6 +2,8 @@
 // Created by wbai on 9/19/2023.
 //
 
+#include <iostream>
+
 /*
  * To prevent recursion, cpp associate a bit with every macro that has been defined.
  * The bit reflects whether the macro is currently being replaced with its substitution
@@ -14,33 +16,66 @@
  * to macro-expand -- because it's a function-like macro not followed by "(" -- cpp still
  * sets the unavailable bit. Moreover, once the unavailable has been set on an input token,
  * it is never cleared.
- * 1.
  */
 
-#include <iostream>
+
+/*
+ * __VA_ARGS__ expands to all arguments passed to the macro,
+ * separated by commas.
+ * In C++20, it introduced __VA_OPT__. The sequence __VA_OPT__(x),
+ * which is only legal in the substitution list of a variable-argument macro,
+ * expands to x if __VA_ARGS__ is none-empty and to nothing if it is empty.
+ */
+#define LOG(fmt, ...) printf("[" fmt "]\n" __VA_OPT__(,) __VA_ARGS__)
+
+/*
+ * As an unintended benefit, differentiating between empty and non-empty
+ * argument lists is exactly the mechanism we need to implement the
+ * base case in recursion
+ */
 
 #define PARENS ()
 
-#define EXPAND4(arg) arg
-#define EXPAND3(arg) EXPAND4(EXPAND4(EXPAND4(EXPAND4(arg))))
+#define EXPAND4(...) __VA_ARGS__
+#define EXPAND3(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND1(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
 
-enum EPOLL_EVENTS {
-    EPOLLIN = 0x001,
-#define EPOLLIN EPOLLIN
-    EPOLLPRI = 0x002,
-#define EPOLLPRI EPOLLPRI
-    EPOLLOUT = 0x003,
-#define EPOLLOUT EPOLLOUT
-};
+#define FOR_EACH(macro, ...)                                    \
+    __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
 
-#define FL(x) ((x) + 1)
-#define ID(arg) arg
-#define ID2(arg) arg
+#define FOR_EACH_HELPER(macro, a1, ...)                         \
+    macro(a1)                                                   \
+    __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+
+#define ENUM_CASE(name) case name: return #name;
+
+#define MAKE_ENUM(type, ...)                    \
+enum type {                                     \
+    __VA_ARGS__                                 \
+};                                              \
+constexpr const char * to_cstring(type _e) {    \
+    using enum type;                            \
+    switch (_e) {                               \
+        FOR_EACH(ENUM_CASE, __VA_ARGS__)        \
+        default:                                \
+            return "unknown";                   \
+    }                                           \
+}
+
+MAKE_ENUM(MyType, ZERO, ONE, TWO, THREE);
+
+void test(MyType e) {
+    std::cout << to_cstring(e) << " = " << e << std::endl;
+}
 
 int main() {
-//    std::cout << EXPAND3("hello") << std::endl;
-//    std::cout << EPOLLIN << std::endl;
-    std::cout << ID(ID)(ID)("test") << std::endl;
-    std::cout << ID(ID2)(ID)("test") << std::endl;
+    test(ZERO);
+    test(ONE);
+    test(TWO);
+    test(THREE);
 }
 
